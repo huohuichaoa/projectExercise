40dbf118ecc015ef86eb342bb2fbd2a5e1b34677
huohuichao <hhc166198043751999@163.com>
2023-03-28 14:10:09 +0800

    first commit

---
+++ src/main/java/com/example/projectexercise/api/Test.java
@@ -0,0 +1,14 @@
+package com.example.projectexercise.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class Test {
+    @GetMapping("/one")
+    public String test(){
+        return "ok";
+    }
+}
